44f2ed094e1e99accbc3487a2d1b8296f1cd2cdd
BHAVADHARANI G <147973983+bhavadharani216@users.noreply.github.com>
2026-09-21 12:09:24 +0530

    Create 9. Palindrome Number.java

---
+++ DAILY LEETCODE PRACTICE/9. Palindrome Number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int temp=x;
+        int sum =0;
+
+        while( x > 0){
+            int rem = x % 10;
+            sum=sum*10+rem;
+            x=x/10;
+        }
+        if(temp == sum){
+            return true;
+        }
+        return false;
+    }
+}
